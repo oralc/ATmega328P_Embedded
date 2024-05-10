4ccda8344c5787ec35b0a460a4e3a4349aeec5b4
Oral Cagil <cagil.oral@ktm.com>
2024-05-10 22:31:01 +0200

    Description of changes made

--- sketch_simulator/sketch_simulator.ino
+++ sketch_simulator/sketch_simulator.ino
@@ -2,6 +2,11 @@
 // git commit -m "Message"
 // git push origin main || GIT_SSL_NO_VERIFY=true git push origin main
 
+// git config http.sslVerify false
+// git config http.sslVerify false
+// Then follow the first 3 steps
+
+
 
 void setup() {
   // put your setup code here, to run once:
