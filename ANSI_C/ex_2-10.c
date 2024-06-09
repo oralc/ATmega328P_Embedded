662248619b01b6fa3fe28e10aacc7aaa6524685f
oralc <oralcagil@gmail.com>
2024-06-09 22:30:17 +0200

    Update README

---
+++ ANSI_C/ex_2-10.c
@@ -0,0 +1,20 @@
+void main (void)
+{
+
+    int farenheit;
+    int celsius;
+    int lower;
+    int upper;
+    int step;
+    lower = 0; /* lower limit */
+    upper = 300;
+    step = 20;
+    farenheit = lower;
+    
+    while ( farenheit <= upper)
+    {
+        celsius =5*( farenheit - 32) / 9;
+        farenheit = farenheit + step;
+        printf ("%d F = %d C\n", farenheit , celsius);
+    }
+}
