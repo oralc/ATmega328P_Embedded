14f9e4a37e122f3d86b8ca56e49d3a7ee9720353
Teo Oral <oralcagil@gmail.com>
2024-10-21 19:01:35 +0200

    Initial commit for env_monitor.c

---
+++ Environmental_Monitoring/env_monitor.c
@@ -0,0 +1,34 @@
+#include "DHT.h"
+
+#define DHTPIN 12      // Pin where the DHT11 is connected
+#define DHTTYPE DHT11 // Define the type of DHT sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements
+  delay(2000);
+
+  // Reading temperature and humidity
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Check if any reads failed
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Print the results
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+}
